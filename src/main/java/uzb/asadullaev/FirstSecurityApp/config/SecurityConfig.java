00fffd171d777352440396052f4879daf6110ba2
Agzam Asadullaev <agzamasadullaev@gmail.com>
2023-04-18 00:25:32 +0500

    first Spring Security App

---
+++ src/main/java/uzb/asadullaev/FirstSecurityApp/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package uzb.asadullaev.FirstSecurityApp.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import uzb.asadullaev.FirstSecurityApp.security.AuthProviderImpl;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfiguration {
+    private final AuthProviderImpl authProvider;
+
+    @Autowired
+    public SecurityConfig(AuthProviderImpl authProvider) {
+        this.authProvider = authProvider;
+    }
+
+    // Настраивает аутентификацию
+    protected void configure(AuthenticationManagerBuilder auth){
+        auth.authenticationProvider(authProvider);
+    }
+}
