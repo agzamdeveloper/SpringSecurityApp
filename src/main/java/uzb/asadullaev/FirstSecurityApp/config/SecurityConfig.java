0343b686c207a4786dec762e512d5fc1b596128d
Agzam Asadullaev <agzamasadullaev@gmail.com>
2023-04-23 23:24:09 +0500

    using csrf and Spring authorisation

--- src/main/java/uzb/asadullaev/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/uzb/asadullaev/FirstSecurityApp/config/SecurityConfig.java
@@ -1,73 +1,51 @@
 package uzb.asadullaev.FirstSecurityApp.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import uzb.asadullaev.FirstSecurityApp.services.PersonDetailsService;
+
 
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig implements WebMvcConfigurer {
-    private final PersonDetailsService personDetailsService;
-
-    @Autowired
-    public SecurityConfig(PersonDetailsService personDetailsService) {
-        this.personDetailsService = personDetailsService;
-    }
 
     @Bean
     SecurityFilterChain web(HttpSecurity http) throws Exception {
         http
-                // ...
-                .csrf().disable()
                 .authorizeHttpRequests(authorize -> {
-                            try {
-                                authorize
-                                        .requestMatchers("/auth/login", "/auth/registration", "/error").permitAll()
-                                        .anyRequest().authenticated()
-                                        .and()
-                                        .formLogin().loginPage("/auth/login")
-                                        .loginProcessingUrl("/process_login")
-                                        .defaultSuccessUrl("/hello", true)
-                                        .failureUrl("/auth/login?error");
-                            } catch (Exception e) {
+                    try {
+                        authorize
+                                .requestMatchers("/admin").hasRole("ADMIN")
+                                .requestMatchers("/auth/login", "/auth/registration", "/error").permitAll()
+                                .anyRequest().hasAnyRole("USER", "ADMIN")
+                                .and()
+                                .formLogin().loginPage("/auth/login")
+                                .loginProcessingUrl("/process_login")
+                                .defaultSuccessUrl("/hello", true)
+                                .failureUrl("/auth/login?error")
+                                .and()
+                                .logout()
+                                .logoutUrl("/logout")
+                                .logoutSuccessUrl("/auth/login");
+                        } catch (Exception e) {
                                 throw new RuntimeException(e);
-                            }
                         }
+                    }
                 );
 
         return http.build();
     }
-//    protected void configure(HttpSecurity http) throws Exception {
-//        // Конфигирируем сам Spring Security
-//        // Конфигирируем авторизацию
-//        http.authorizeHttpRequests()
-//                .requestMatchers("/auth/login", "/error").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin().loginPage("/auth/login")
-//                .loginProcessingUrl("/process_login")
-//                .defaultSuccessUrl("/hello", true)
-//                .failureUrl("/auth/login?error");
-//    }
-
-    // Настраиваем аутентификацию
-//    protected void configure (AuthenticationManagerBuilder auth) throws Exception{
-//        auth.userDetailsService(personDetailsService);
-//    }
 
     @Bean
     public PasswordEncoder getPasswordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
-
 
 }
--- src/main/java/uzb/asadullaev/FirstSecurityApp/services/RegistrationService.java
+++ src/main/java/uzb/asadullaev/FirstSecurityApp/services/RegistrationService.java
@@ -1,6 +1,7 @@
 package uzb.asadullaev.FirstSecurityApp.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import uzb.asadullaev.FirstSecurityApp.models.Person;
@@ -9,14 +10,19 @@ import uzb.asadullaev.FirstSecurityApp.repositories.PeopleRepository;
 @Service
 public class RegistrationService {
     private final PeopleRepository peopleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationService(PeopleRepository peopleRepository) {
+    public RegistrationService(PeopleRepository peopleRepository, PasswordEncoder passwordEncoder) {
         this.peopleRepository = peopleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void register(Person person){
+        String encodedPassword = passwordEncoder.encode(person.getPassword());
+        person.setPassword(encodedPassword);
+        person.setRole("ROLE_USER");
         peopleRepository.save(person);
     }
 }
